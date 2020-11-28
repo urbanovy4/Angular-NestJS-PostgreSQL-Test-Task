e063844a67c836f5a3e1b8b48b0ff4f370d9f51b
urbanovy4 <urbanovy4@users.noreply.github.com>
2020-11-28 13:22:01 +0200

    add synchronize

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,67 +1,69 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import {TypeOrmModuleOptions} from '@nestjs/typeorm';
 
 require('dotenv').config();
 
 class ConfigService {
 
-  constructor(private env: { [k: string]: string | undefined }) { }
-
-  private getValue(key: string, throwOnMissing = true): string {
-    const value = this.env[key];
-    if (!value && throwOnMissing) {
-      throw new Error(`config error - missing env.${key}`);
+    constructor(private env: { [k: string]: string | undefined }) {
     }
 
-    return value;
-  }
+    private getValue(key: string, throwOnMissing = true): string {
+        const value = this.env[key];
+        if (!value && throwOnMissing) {
+            throw new Error(`config error - missing env.${key}`);
+        }
 
-  public ensureValues(keys: string[]) {
-    keys.forEach(k => this.getValue(k, true));
-    return this;
-  }
+        return value;
+    }
 
-  public getPort() {
-    return this.getValue('PORT', true);
-  }
+    public ensureValues(keys: string[]) {
+        keys.forEach(k => this.getValue(k, true));
+        return this;
+    }
 
-  public isProduction() {
-    const mode = this.getValue('MODE', false);
-    return mode != 'DEV';
-  }
+    public getPort() {
+        return this.getValue('PORT', true);
+    }
 
-  public getTypeOrmConfig(): TypeOrmModuleOptions {
-    return {
-      type: 'postgres',
+    public isProduction() {
+        const mode = this.getValue('MODE', false);
+        return mode != 'DEV';
+    }
 
-      host: this.getValue('POSTGRES_HOST'),
-      port: parseInt(this.getValue('POSTGRES_PORT')),
-      username: this.getValue('POSTGRES_USER'),
-      password: this.getValue('POSTGRES_PASSWORD'),
-      database: this.getValue('POSTGRES_DATABASE'),
+    public getTypeOrmConfig(): TypeOrmModuleOptions {
+        return {
+            type: 'postgres',
 
-      entities: ['dist/**/*.entity{.ts,.js}'],
+            host: this.getValue('POSTGRES_HOST'),
+            port: parseInt(this.getValue('POSTGRES_PORT')),
+            username: this.getValue('POSTGRES_USER'),
+            password: this.getValue('POSTGRES_PASSWORD'),
+            database: this.getValue('POSTGRES_DATABASE'),
 
-      migrationsTableName: 'migration',
+            entities: ['dist/**/*.entity{.ts,.js}'],
 
-      migrations: ['src/migration/*.ts'],
+            synchronize: true,
+            migrationsTableName: 'migration',
 
-      cli: {
-        migrationsDir: 'src/migration',
-      },
+            migrations: ['src/migration/*.ts'],
 
-      ssl: this.isProduction(),
-    };
-  }
+            cli: {
+                migrationsDir: 'src/migration',
+            },
+
+            ssl: this.isProduction(),
+        };
+    }
 
 }
 
 const configService = new ConfigService(process.env)
-  .ensureValues([
-    'POSTGRES_HOST',
-    'POSTGRES_PORT',
-    'POSTGRES_USER',
-    'POSTGRES_PASSWORD',
-    'POSTGRES_DATABASE'
-  ]);
+    .ensureValues([
+        'POSTGRES_HOST',
+        'POSTGRES_PORT',
+        'POSTGRES_USER',
+        'POSTGRES_PASSWORD',
+        'POSTGRES_DATABASE'
+    ]);
 
-export { configService };
+export {configService};
